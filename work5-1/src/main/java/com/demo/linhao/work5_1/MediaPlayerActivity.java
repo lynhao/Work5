7f399b50c12841b5814b68e6dc0024db1f392f86
linhao <626607984>
2015-11-27 13:04:59 +0800

    Initial commit

---
+++ work5-1/src/main/java/com/demo/linhao/work5_1/MediaPlayerActivity.java
@@ -0,0 +1,278 @@
+package com.demo.linhao.work5_1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by linhao on 15/11/24.
+ */
+public class MediaPlayerActivity extends Activity {
+    private Display currDisplay;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private MediaPlayer player;
+    private  int vWidth,vHeight;
+    private Timer timer;
+    private ImageButton rew;//后退
+    private ImageButton pause;//暂停
+    private ImageButton start;//开始
+    private ImageButton ff;//快速
+    private TextView play_time;//播放时间
+    private TextView all_time;//总播放时间
+    private TextView title;//播放文件名称
+    private SeekBar seekbar;//进度条
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//隐藏标题
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        setContentView(R.layout.main);
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        String mPath = "/mnt/sdcard/music.mp3";
+        if (uri!=null)
+        {
+            mPath=uri.getPath();//获得媒体路径
+        }else {
+            //从多媒体文件预览传来的点播文件路径
+            Bundle bundle = getIntent().getExtras();
+            if (bundle!=null)
+            {
+                String t_path = bundle.getString("path");
+                if (t_path!=null && !"".equals(t_path))
+                {
+                    mPath=t_path;
+                }
+            }
+        }
+        title = (TextView) findViewById(R.id.titles);
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceview);
+        rew = (ImageButton) findViewById(R.id.rew);
+        pause = (ImageButton) findViewById(R.id.pause);
+        start = (ImageButton) findViewById(R.id.start);
+        ff = (ImageButton) findViewById(R.id.ff);
+
+        play_time = (TextView) findViewById(R.id.play_time);
+        all_time = (TextView) findViewById(R.id.all_time);
+        seekbar = (SeekBar) findViewById(R.id.seekbar);
+
+        //给SurfaceView添加CallBack监听器
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                //设置MediaPlayer在指定的Surface中进行播放
+                player.setDisplay(surfaceHolder);
+                //在指定了MediaPlayer播放器后，使用prepareAsync准备播放
+                player.prepareAsync();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                //当SurfaceView中得Surface被创建时调用
+            }
+        });
+        //为了可以播放视频或者使用Camera预览，需要指定Buffer类型
+        surfaceHolder.setType(surfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        //下面开始实例化MediaPlayer对象
+        player = new MediaPlayer();
+        //设置播放完成监听器
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                if (timer != null) {
+                    timer.cancel();
+                    timer = null;
+                }
+            }
+        });
+        //设置prepare完成监听器
+        player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                //当prepare完成后，该方法触发用于播放器
+                //首先取得食品的宽和高
+                vWidth = player.getVideoWidth();
+                vHeight = player.getVideoHeight();
+                if (vWidth > currDisplay.getWidth() || vHeight > currDisplay.getHeight()) {
+                    //如果视频的宽或者高超出了屏幕的大小，则进行缩放
+                    float wRadio = (float) vWidth / (float) currDisplay.getWidth();
+                    float hRadio = (float) vHeight / (float) currDisplay.getHeight();
+                    //选择大得比例进行缩放
+                    float radio = Math.max(wRadio, hRadio);
+                    vWidth = (int) Math.ceil((float) vWidth / radio);
+                    vHeight = (int) Math.ceil((float) vHeight / radio);
+                    //重新设置surfaceview的布局参数
+                    surfaceView.setLayoutParams(new LinearLayout.LayoutParams(vWidth, vHeight));
+                    //然后开始播放视频
+                    player.start();
+                } else {
+                    player.start();
+                }
+                if (timer != null) {
+                    timer.cancel();
+                    timer = null;
+                }
+                //启动时间更新及进度更新任务，没0.5s更新一次
+                timer = new Timer();
+                timer.schedule(new MyTask(), 50, 500);
+            }
+        });
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            title.setText(mPath.substring(mPath.lastIndexOf("/") + 1));
+            player.setDataSource(mPath);
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        //暂停操作
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pause.setVisibility(View.GONE);
+                start.setVisibility(View.VISIBLE);
+                player.pause();
+                if (timer!=null)
+                {
+                    timer.cancel();
+                    timer = null;
+                }
+            }
+        });
+        //播放操作
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //按下开始操作后，pause设为可见，start设为隐藏
+                start.setVisibility(View.GONE);
+                pause.setVisibility(View.VISIBLE);
+                //启动播放
+                player.start();
+                if (timer!=null)
+                {
+                    timer.cancel();
+                    timer = null;
+                }
+                //启动时间更新及进度条更新任务，每0.5s更新一次
+                timer = new Timer();
+                timer.schedule(new MyTask(),50,500);
+            }
+        });
+        //快退操作，每次快退10秒
+        rew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //判断是否正在播放
+                if (player.isPlaying())
+                {
+                    int currentPosition = player.getCurrentPosition();
+                    if (currentPosition-10000>0)
+                    {
+                        player.seekTo(currentPosition-10000);
+                    }
+                }
+            }
+        });
+        //快进操作，每次快进10s
+        ff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //判断是否在播放
+                if (player.isPlaying())
+                {
+                    int currentPosition = player.getCurrentPosition();
+                    if (currentPosition+10000<player.getDuration())
+                    {
+                        player.seekTo(currentPosition+10000);
+                    }
+                }
+            }
+        });
+        //取得当前Display对象
+        currDisplay = this.getWindowManager().getDefaultDisplay();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,1,0,"文件夹");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==1)
+        {
+            Intent intent = new Intent(MediaPlayerActivity.this,MyFileActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //进度栏分类
+    public class MyTask extends TimerTask{
+        @Override
+        public void run() {
+            Message message = new Message();
+            message.what=1;
+            //发生消息更新进度栏和时间显示
+            handler.sendMessage(message);
+        }
+    }
+    //处理进度栏和时间显示
+    private final Handler handler = new Handler(){
+        public void handleMessage(Message msg)
+        {
+            switch (msg.what)
+            {
+                case 1:
+                    Time progeress = new Time(player.getCurrentPosition());
+                    Time allTime = new Time(player.getDuration());
+                    String timeStr = progeress.toString();
+                    String timeStr2 = allTime.toString();
+                    //已播放时间
+                    play_time.setText(timeStr.substring(timeStr.indexOf(":")+1));
+                    //总时间
+                    all_time.setText(timeStr2.substring(timeStr.indexOf(":")+1));
+                    int progressValue = 0;
+                    if (player.getDuration()>0)
+                    {
+                        progressValue = seekbar.getMax()*player.getCurrentPosition()/player.getDuration();
+                    }
+                    //进度栏进度
+                    seekbar.setProgress(progressValue);
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    };
+}
---
+++ work5-1/src/main/java/com/demo/linhao/work5_1/MyFileActivity.java
@@ -0,0 +1,228 @@
+package com.demo.linhao.work5_1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.Vector;
+
+/**
+ * Created by linhao on 15/11/24.
+ */
+public class MyFileActivity extends Activity {
+    //支持的媒体格式
+    private final String[]FILE_MapTable={
+            ".3gp",".mov",".avi",".rmvb",".wmv",".mp3",".mp4"
+    };
+    private Vector<String> items = null;//items存放显示的名称
+    private Vector<String> paths = null;//paths存放文件路径
+    private Vector<String> sizes = null;//sizes存放文件大小
+    private String rootPath = "/mnt/sdcard";//起始文件夹
+    private EditText pathEditext; //路径
+    private Button queryButton;//查询按钮
+    private ListView fileListView;//文件列表
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setTitle("多媒体文件浏览");
+        setContentView(R.layout.myfile);
+        pathEditext = (EditText) findViewById(R.id.path_edit);
+        queryButton = (Button) findViewById(R.id.qry_button);
+        fileListView = (ListView) findViewById(R.id.file_listview);
+        //单击查询点击事件
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                File file = new File(pathEditext.getText().toString());
+                if (file.exists())
+                {
+                    if (file.isFile())
+                    {
+                        //如果是媒体文件则直接打开播放
+                        openFile(pathEditext.getText().toString());
+                    }else{
+                        //如果是目录则打开目录下得文件
+                        getFilesDir(pathEditext.getText().toString());
+                    }
+                }else{
+                    Toast.makeText(MyFileActivity.this,"找不到该位置，请确定位置是否正确",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        //设置ListItem中的文件被单击时要做的动作
+        fileListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                fileOrDir(paths.get(i));
+            }
+        });
+        //打开默认文件夹
+        getFilesDir();
+    }
+    /**
+     * 重写返回键功能：返回上一级文件夹
+     *
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //判断触发案件是否为back键
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            pathEditext = (EditText) findViewById(R.id.path_edit);
+            File file = new File(pathEditext.getText().toString());
+            if (rootPath.equals(pathEditext.getText().toString().trim()))
+            {
+                return super.onKeyDown(keyCode,event);
+            }else
+            {
+                getFilesDir(file.getParent());
+                return true;
+            }
+            //如果不是back键则正常响应
+        }else
+        {
+            return super.onKeyDown(keyCode, event);
+        }
+
+    }
+    /**
+     *  处理文件或目录的方法
+     */
+    private void fileOrDir(String path){
+        File file = new File(path);
+        if (file.isDirectory())
+        {
+            getFilesDir(file.getPath());
+        }else{
+            openFile(path);
+        }
+    }
+    /**
+     * 取得文件结构的方法
+     */
+    private void getFilesDir(String filePath){
+        //设置目前所在的路径
+        pathEditext.setText(filePath);
+        items = new Vector<String>();
+        paths = new Vector<String>();
+        sizes = new Vector<String>();
+        File f =  new File(filePath);
+        File[] files = f.listFiles();
+        if (files!=null)
+        {
+            //将所有文件添加到ArrayList中
+            for (int i = 0;i<files.length;i++)
+            {
+                if (files[i].isDirectory())
+                {
+                    items.add(files[i].getName());
+                    paths.add(files[i].getPath());
+                    sizes.add("");
+                }
+            }
+            for (int i = 0;i<files.length;i++)
+            {
+                if (files[i].isFile())
+                {
+                    String fileName = files[i].getName();
+                    int index = fileName.lastIndexOf(".");
+                    if (index>0)
+                    {
+                        String endName = fileName.substring(index,fileName.length()).toLowerCase();
+                        String type = null;
+                        for (int x = 0;x<FILE_MapTable.length;x++)
+                        {
+                            //符合预先定义的多媒体格式的文件才会再界面显示
+                            if (endName.equals(FILE_MapTable[x]))
+                            {
+                                type = FILE_MapTable[x];
+                                break;
+                            }
+                        }
+                        if (type != null)
+                        {
+                            items.add(files[i].getName());
+                            paths.add(files[i].getPath());
+                            sizes.add(files[i].length()+"");
+                        }
+                    }
+                }
+            }
+        }
+        //使用自定义的FileListAdapter将数据传入ListView中
+
+    }
+    /*打开媒体文件*/
+    private void openFile(String path)
+    {
+        //打开媒体播放器，进行播放
+        Intent intent = new Intent(MyFileActivity.this,MediaPlayerActivity.class);
+        intent.putExtra("path",path);
+        startActivity(intent);
+        finish();
+    }
+    /**
+     * ListView列表适配器
+     */
+    class FileListAdapter extends BaseAdapter
+    {
+        private Vector<String> items = null;//存放显示的名称
+        private MyFileActivity myFile;
+        private FileListAdapter(MyFileActivity myFile,Vector<String> items){
+            this.items = items;
+            this.myFile = myFile;
+        }
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return items.elementAt(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View convertview, ViewGroup viewGroup) {
+            if (convertview==null)
+            {
+                convertview = myFile.getLayoutInflater().inflate(R.layout.file_item,null);
+            }
+            //文件名称
+            TextView name = (TextView) convertview.findViewById(R.id.name);
+            //媒体文件类型
+            ImageView music = (ImageView) convertview.findViewById(R.id.music);
+            //文件类型
+            ImageView folder = (ImageView) convertview.findViewById(R.id.folder);
+            name.setText(items.elementAt(i));
+            if (sizes.elementAt(i).equals(""))
+            {
+                //隐藏媒体文件图标，显示文件夹图标
+                music.setVisibility(View.GONE);
+                folder.setVisibility(View.VISIBLE);
+            }else
+            {
+                folder.setVisibility(View.GONE);
+                music.setVisibility(View.VISIBLE);
+            }
+            return convertview;
+        }
+    }
+}
